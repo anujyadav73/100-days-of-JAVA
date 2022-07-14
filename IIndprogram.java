39de634535ab4819b2d395405585e22ebffa810c
anujyadav73 <92756623+anujyadav73@users.noreply.github.com>
2022-07-14 16:52:11 +0530

    Add files via upload

---
+++ IIndprogram.java
@@ -0,0 +1,12 @@
+
+//Java Variable Example: Add Two Numbers
+public class IIndprogram{    
+public static void main(String[] args){    
+int a=10;    
+int b=10;    
+int c=a+b;    
+System.out.println(c);    
+}  
+}
+
+//OUTPUT : 20
