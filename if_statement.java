62d440c003049da4e2c142d6752c2c0fb8aba8ed
anujyadav73 <92756623+anujyadav73@users.noreply.github.com>
2022-07-16 23:23:46 +0530

    Add files via upload

---
+++ if_statement.java
@@ -0,0 +1,22 @@
+public class if_statement {
+    public static void main(String[] args) {
+        int x,y;
+        x = 10;
+        y = 20;
+        if(x<y) System.out.println("x is less than y");
+        x = x*2 ;
+        if (x== y)System.out.println("x now equal to y");
+        x = x*2;
+        if(x>y) System.out.println("x is now greater than y");
+
+    }
+    
+}
+
+
+
+
+//Output -
+//x is less than y
+//x now equal to y
+//x is now greater than y
