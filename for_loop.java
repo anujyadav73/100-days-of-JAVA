064482f74696efc90987fab6f02cd1b268ffa0c6
anujyadav73 <92756623+anujyadav73@users.noreply.github.com>
2022-07-17 19:05:05 +0530

    Add files via upload

---
+++ for_loop.java
@@ -0,0 +1,19 @@
+// Program to print a text 5 times
+
+public class for_loop {
+    public static void main(String[] args) {
+  
+        int n = 5;
+        // for loop  
+        for (int i = 1; i <= n; ++i) {
+          System.out.println("Java is fun");
+        }
+      }
+    
+}
+/* Output
+Java is fun
+Java is fun
+Java is fun
+Java is fun
+Java is fun*/
